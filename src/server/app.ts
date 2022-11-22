b9a9c9db2e2725ed1e87a7cae21cce3a2ad6214b
Víctor Merino <victor.cornella.86@gmail.com>
2022-11-22 15:35:11 +0100

    Add CORS packaje and set it up in app.ts

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,10 +1,22 @@
 import express from "express";
 import morgan from "morgan";
+import cors from "cors";
+import environment from "./loadEnvironment.js";
+
+const { corsAllowedDomain } = environment;
 
 const app = express();
 
 app.disable("x-powered-by");
-app.use(express.json());
+const allowedOrigins = [corsAllowedDomain];
+
+const options: cors.CorsOptions = { origin: allowedOrigins };
+
+app.use(cors(options));
 app.use(morgan("dev"));
+app.use(express.json());
+app.use("/", (req, res) => {
+  res.send("It is working");
+});
 
 export default app;
