02bda659f905fa759c2717ba3b5da40e41818dd6
Víctor Merino <victor.cornella.86@gmail.com>
2022-11-28 23:13:30 +0100

    Create Recipe database model

---
+++ src/database/models/Recipe.ts
@@ -0,0 +1,37 @@
+import { model, Schema } from "mongoose";
+
+export const recipeSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  ingredients: {
+    type: [String],
+    required: true,
+  },
+  steps: {
+    type: [String],
+    required: true,
+  },
+  imageBackup: {
+    type: String,
+    required: true,
+  },
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+});
+
+const Recipe = model("Recipe", recipeSchema, "recipes");
+
+export default Recipe;
