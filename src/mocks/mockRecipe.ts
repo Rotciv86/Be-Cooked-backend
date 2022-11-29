178a72969e0ef1f62270057a62574c66491ad072
Víctor Merino <victor.cornella.86@gmail.com>
2022-11-29 15:37:52 +0100

    Add getAllRecipes unitary test

---
+++ src/mocks/mockRecipe.ts
@@ -0,0 +1,10 @@
+export const mockRecipe = {
+  name: "Arroz caldoso con bogavante",
+  category: "arroces",
+  image: "https://i.blogs.es/9c8da4/arroz-caldoso-con-bogavante/1366_2000.jpg",
+  ingredients: ["arroz", "bogavante", "perejil"],
+  steps: ["calentar el agua", "echar el arroz", "remover con mucho amor"],
+  imageBackup:
+    "https://i.blogs.es/9c8da4/arroz-caldoso-con-bogavante/1366_2000.jpg",
+  owner: "637d2b1f6c35b3212b9a74c4",
+};
