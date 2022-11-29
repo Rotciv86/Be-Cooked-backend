5c54ad4de65bd588178ff9e8bc0bb134d6be9fbb
Víctor Merino <victor.cornella.86@gmail.com>
2022-11-29 18:48:38 +0100

    Add recipes endpoint integration test with 3 possible cases

---
+++ src/server/routes/recipesRouters/recipesRouter.test.ts
@@ -0,0 +1,67 @@
+import { MongoMemoryServer } from "mongodb-memory-server";
+import request from "supertest";
+import mongoose from "mongoose";
+import app from "../../app";
+import databaseConnection from "../../../database/databaseConnection";
+import Recipe from "../../../database/models/Recipe";
+
+let server: MongoMemoryServer;
+
+beforeAll(async () => {
+  await mongoose.disconnect();
+  server = await MongoMemoryServer.create();
+  await databaseConnection(server.getUri());
+});
+
+beforeEach(async () => {
+  await Recipe.deleteMany({});
+});
+
+afterAll(async () => {
+  await mongoose.disconnect();
+  await server.stop();
+});
+
+describe("Given a GET /recipes/list", () => {
+  describe("When it receives a request with an empty body and 10 recipes in the data base", () => {
+    test("Then it should respond with a 200 status", async () => {
+      const status = 200;
+
+      const response = await request(app).get("/recipes/list").expect(status);
+
+      expect(response.body).toHaveProperty("recipes");
+    });
+  });
+
+  describe("When it receives a request with no recipes in the data base", () => {
+    test("Then it should respond with a 200 status and a object with property 'recipes' that has an array of empty recipes", async () => {
+      const status = 200;
+
+      Recipe.find = jest.fn().mockReturnValue(null);
+
+      const response = await request(app).get("/recipes/list").expect(status);
+
+      expect(response.body).toStrictEqual({
+        recipes: [
+          {
+            recipes: null,
+          },
+        ],
+      });
+    });
+  });
+
+  describe("When it receives a request and an interval server error happens", () => {
+    test("Then it should return a 500 status", async () => {
+      const status = 500;
+
+      Recipe.find = jest
+        .fn()
+        .mockRejectedValue({ error: "General error server" });
+
+      const response = await request(app).get("/recipes/list").expect(status);
+
+      expect(response.body).toStrictEqual({ error: "General error server" });
+    });
+  });
+});
